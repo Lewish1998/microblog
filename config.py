0132a04e9d5932d4aac04e452d3b1ff0a0d53837
Lewish1998 <108507629+Lewish1998@users.noreply.github.com>
2024-04-04 00:04:36 +0100

    temp email config

--- config.py
+++ config.py
@@ -1,5 +1,8 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev'
@@ -13,4 +16,13 @@ class Config:
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['lewishalstead5@gmail.com']
     
+    # MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    # MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    # MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    # MAIL_USERNAME = os.getenv('EMAIL_USERNAME')
+    # MAIL_PASSWORD = os.getenv('EMAIL_PASSWORD')
+    
+    
+    
+    'gou7685&FD&67sdf8f^*f7f76f8F^*F5'
     POSTS_PER_PAGE = 10
