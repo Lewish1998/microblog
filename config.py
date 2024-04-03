a8a7de66cadb457bc325b95eafb4ac83238e5ad0
Lewish1998 <108507629+Lewish1998@users.noreply.github.com>
2024-04-04 00:06:42 +0100

    admin email

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ class Config:
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['lewishalstead5@gmail.com']
+    ADMINS = os.getenv('ADMIN_EMAIL_LIST')
     
     # MAIL_SERVER = os.environ.get('MAIL_SERVER')
     # MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
