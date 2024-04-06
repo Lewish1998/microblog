09266f88e5612d3be37d632ca8c10a3bf3bb8eaa
Lewish1998 <108507629+Lewish1998@users.noreply.github.com>
2024-04-06 13:34:25 +0100

    smtp

--- config.py
+++ config.py
@@ -9,20 +9,27 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
         
-    MAIL_SERVER = os.environ.get('MAIL_SERVER')
-    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
-    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = os.getenv('ADMIN_EMAIL_LIST')
-    
     # MAIL_SERVER = os.environ.get('MAIL_SERVER')
     # MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
     # MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
-    # MAIL_USERNAME = os.getenv('EMAIL_USERNAME')
-    # MAIL_PASSWORD = os.getenv('EMAIL_PASSWORD')
+    # MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    # MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = os.getenv('ADMINS')
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.getenv('EMAIL_USERNAME')
+    MAIL_PASSWORD = os.getenv('EMAIL_PASSWORD')
+    
+
     
     
     
     'gou7685&FD&67sdf8f^*f7f76f8F^*F5'
     POSTS_PER_PAGE = 10
+    
+    
+# ADMINS = os.getenv('ADMINS')
+# MAIL_USERNAME = os.getenv('EMAIL_USERNAME')
+
+# print(ADMINS)
